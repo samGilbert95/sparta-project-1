0f9cf4ad156c7e2dbf9c7ed1a01a5adb827dcb3b
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-09-17 13:39:10 +0100

    Added song information and randomiser

--- js/app.js
+++ js/app.js
@@ -4,16 +4,51 @@ document.addEventListener('DOMContentLoaded',() => {
   const gridArea = document.getElementById('gridSect');
   let gridArray = [];
   let removed = [];
-// create a large grid over the album imageGrid
-// This will be the baseline at the start of the game
+  let songArray = [
+    ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+    ["George Ezra","Shotgun","images/shotgun.jpeg","Ezra.mp3"],
+    ["Marshmello","Happier","images/Marshmello.png","marsh.mp3"],
+    ["Childish Gambino","Feels Like Summer","images/gambino.jpg","gambino.mp3"],
+    ["Foster The People","Pumped Up Kicks","images/foster.png","kicks.mp3"],
+    ["Gorillaz","Humility","images/gorillaz.jpg","gorillaz.mp3"]
+    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+  ];
+
   const imageGrid = new Object();
 
+  imageGrid.getSong = () => {
+    let rand = Math.floor(Math.random()*songArray.length);
+    console.log(rand);
+    let randArtist = songArray[rand][0];
+    let randSongName = songArray[rand][1];
+    let randSongImg = songArray[rand][2];
+    let randSongMp3 = songArray[rand][3];
+    const music = document.getElementById('musicPlayer');
+    const cover = document.getElementById('albumArea');
+    const songname = document.getElementById('songNameTest');
+    console.log(randSongName);
+    console.log(randSongImg);
+    console.log(randSongMp3);
+    console.log(music);
+    music.setAttribute("src","mp3/"+randSongMp3);
+    cover.setAttribute("src",randSongImg);
+    songname.innerHTML = randSongName + ' by ' + randArtist;
+    music.play();
+
+  }
+
+  // create a large grid over the album imageGrid
+  // This will be the baseline at the start of the game
+  // also calls get song to display img and sound
   imageGrid.populate = () => {
-    for (let i = 0;i <5;i++){
+    for (let i = 0;i <10;i++){
       const newBlock = document.createElement('div');
       newBlock.setAttribute('class','row');
       gridArea.appendChild(newBlock);
-      for (var j = 0; j< 5;j++){
+      for (var j = 0; j< 10;j++){
         const test = document.createElement('div');
         test.setAttribute('class','block');
         newBlock.appendChild(test);
@@ -26,20 +61,23 @@ document.addEventListener('DOMContentLoaded',() => {
   // will need 2d array of all block objects
   // function on loop set to time delay
   imageGrid.deleteRand= () => {
-    console.log(gridArray);
     setInterval(function () {
-      let rand = Math.floor(Math.random()*25);
+      let rand = Math.floor(Math.random()*100);
       if (removed.indexOf(rand) == -1) {
         removed.push(rand);
         const element = gridArray[rand];
         element.classList.remove('block');
         element.classList.add('blockInv');
       }
-      console.log(rand);
-      console.log(removed);
     }, 1000);
   }
 
+  // adds 4 buttons underneath the grid, one of which is the answer
+  // imageGrid.addOptions = () => {
+  //
+  // }
+
   imageGrid.populate();
+  imageGrid.getSong();
   imageGrid.deleteRand();
 });
