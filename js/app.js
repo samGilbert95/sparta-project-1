49ca6bf51db926f2084228d13df729942ba0fe4b
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-09-18 10:00:53 +0100

    Changed score + fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded',() => {
+  //============================VARIABLES=====================
   const gridArea = document.getElementById('gridSect');
+  const success = document.getElementById('correct');
+  const fail = document.getElementById('wrong');
   let gridArray = [];
   let removed = [];
   let timer = 0;
   let songCount = 0;
+  let timecount = 0;
   let songName = '';
   let songMax = 5;
   let scores = [];
-  let dispSong = songCount + 1;
+  let dispSong = 0;
   let songArray = [
     ["Adele","rolling in the deep","images/adele21.png","Adele.mp3"],
     ["George Ezra","shotgun","images/shotgun.jpeg","Ezra.mp3"],
@@ -15,21 +19,27 @@ document.addEventListener('DOMContentLoaded',() => {
     ["Childish Gambino","feels like summer","images/gambino.jpg","gambino.mp3"],
     ["Foster The People","pumped up kicks","images/foster.png","kicks.mp3"],
     ["Gorillaz","humility","images/gorillaz.jpg","gorillaz.mp3"]
-    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
-    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
-    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
-    // ["Adele","Rolling In The Deep","images/adele21.png","Adele.mp3"],
+    ["U2","beautiful day","images/u2.jpg","day.mp3"],
+    ["Gnarls Barkley","crazy","images/crazy.JPG","crazy.mp3"],
+    ["Daft Punk","one more time","images/daft.jpg","daft.mp3"],
+    ["The Walkmen","the rat","images/rat.jpg","rat.mp3"],
+    ["Capital Cities","safe and sound","images/safe.jpg","safe.mp3"],
+    ["Pharell Williams","happy","images/happy.jpg","happy.mp3"],
+    ["Mark Ronson","uptown funk","images/funk.jpeg","funk.mp3"],
   ];
   let score = 0;
-
+  //============================OBJDEFINITIONS=====================
   const imageGrid = new Object();
   const leaderboard = new Object();
+
+  //============================IMAGEGRID==========================
 
   imageGrid.guessBtn = document.getElementById('guessBtn');
   imageGrid.nextSong = () => {
     $('.row').remove();
     gridArray = [];
     removed = [];
+    timecount = 0;
     console.log('Next Song');
     clearInterval(timer);
     imageGrid.populate();
@@ -49,6 +59,8 @@ document.addEventListener('DOMContentLoaded',() => {
         gridArray.push(newElement);
       }
     }
+    success.setAttribute('style','visibility:hidden');
+    fail.setAttribute('style','visibility:hidden');
     imageGrid.getSong();
   }
 
@@ -65,6 +77,7 @@ document.addEventListener('DOMContentLoaded',() => {
     const songname = document.getElementById('songNameTest');
     music.setAttribute("src","mp3/"+randSongMp3);
     cover.setAttribute("src",randSongImg);
+    dispSong++;
     songName = randSongName;
     stage.innerHTML = "Song " + dispSong + " of " + songMax;
     music.play();
@@ -83,7 +96,17 @@ document.addEventListener('DOMContentLoaded',() => {
         element.classList.remove('block');
         element.classList.add('blockInv');
       }
+      console.log(timer);
+      timecount++;
     }, 1000);
+  }
+
+  imageGrid.success = () => {
+    success.setAttribute('style','visibility:visable');
+  }
+
+  imageGrid.fail = () => {
+    fail.setAttribute('style','visibility:visable');
   }
 
   //checks if user input is correct.
@@ -97,23 +120,30 @@ document.addEventListener('DOMContentLoaded',() => {
     let guess = document.getElementById('guess');
     if (guess.value.toLowerCase() == songName){
       console.log('Correct!');
-      score++;
+      score = score + (100-(timecount*3));
+      imageGrid.success();
       message.innerHTML = 'Score: ' + score;
     } else {
       console.log('Incorrect!');
+      imageGrid.fail();
     }
-    if (songCount === songMax){
+    if (songCount >= songMax){
       console.log(score);
       const name = prompt('Enter Your Name');
       localStorage.setItem(name,score);
+      setTimeout(function(){
+        document.location.href = 'index.html';
+      }, 10);
     }
     setTimeout(function(){
       imageGrid.nextSong();
-    }, 10);
+    }, 1000);
   }
 
-  // Sorts array by score value and prints out to Leaderboard
+  // ======================LEADERBOARD===============================
 
+  // Sorts array by score value and prints out to Leaderboard
+  leaderboard.clearBtn = document.getElementById('clearStore');
   leaderboard.getScores = () => {
     console.log('working');
     for (var i = 0; i < localStorage.length; i++){
@@ -127,6 +157,10 @@ document.addEventListener('DOMContentLoaded',() => {
     console.log(scores);
   }
 
+  leaderboard.clearLead = () => {
+    localStorage.clear();
+  }
+
   leaderboard.printScores = () => {
     leaderboard.getScores();
     scores.sort(function(a,b){
@@ -138,8 +172,7 @@ document.addEventListener('DOMContentLoaded',() => {
         return 0;
       }
     });
-    console.log(scores);
-    for (var i = 0; i < 3; i++){
+    for (var i = 0; i < localStorage.length; i++){
       var node = document.createElement("li");
       var textnode = document.createTextNode(scores[i]);
       node.appendChild(textnode);
@@ -151,8 +184,11 @@ document.addEventListener('DOMContentLoaded',() => {
     imageGrid.guessBtn.addEventListener('click',(e) => {
         imageGrid.checkAnswer();
     });
-  } else {
+  } else if (document.title == 'Leaderboard') {
     leaderboard.printScores();
+    leaderboard.clearBtn.addEventListener('click',(e) => {
+        leaderboard.clearLead();
+    });
   }
 
 });
